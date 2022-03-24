17624d5efbe469afb17fe7ae497f540ab4f9be49
Gguinea <Gguinea>
2022-03-24 19:12:32 +0100

    Scaffold

---
+++ src/main/java/movie/movieShow/MovieShowApplication.java
@@ -0,0 +1,13 @@
+package movie.movieShow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieShowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/movie/movieShow/MovieShowApplicationTests.java
@@ -0,0 +1,13 @@
+package movie.movieShow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieShowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
